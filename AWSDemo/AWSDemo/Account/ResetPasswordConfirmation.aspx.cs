21d9785c3bd15276b220685ddd1e1dce8ccbcb3c
tientran <tientran@tekcent.net>
2016-07-06 10:57:26 +0700

    Demo solution

---
+++ AWSDemo/AWSDemo/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace AWSDemo.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ AWSDemo/AWSDemo/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AWSDemo
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var connectionString  = ConfigurationManager.ConnectionStrings["AWS"].ConnectionString;
+            var query = "Select * from Author";
+            var con = new SqlConnection(connectionString);
+            con.Open();
+            var da = new SqlDataAdapter(query, con);
+            var ds = new DataSet();
+
+            da.Fill(ds);
+        }
+    }
+}
